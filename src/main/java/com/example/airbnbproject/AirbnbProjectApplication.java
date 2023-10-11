484531f01a77902fbfa1de8657eb4d9a4691f64d
BenKim95 <forseminaar@gmail.com>
2023-10-11 10:36:45 +0900

    Airbnb 클론코딩 init commit

---
+++ src/main/java/com/example/airbnbproject/AirbnbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.airbnbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class AirbnbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbProjectApplication.class, args);
+    }
+}
